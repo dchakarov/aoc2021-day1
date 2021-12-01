afbee36c1751b09bb12a2dc5eee18640b3a1f889
Dimitar Chakarov <dimitar.chakarov@just-eat.com>
2021-12-01 14:37:10 +0000

    My day 1 solution

--- Advent of Code/main.swift
+++ Advent of Code/main.swift
@@ -12,28 +12,38 @@ func main() {
     
     let lines = inputString.components(separatedBy: "\n")
         .filter { !$0.isEmpty }
-    
-    // Sample algorithm
-    var scoreboard = [String: Int]()
-    lines.forEach { line in
-        let (name, score) = parseLine(line)
-        scoreboard[name] = score
+        .map { Int($0)! }
+
+    var increased = 0
+    for i in 1 ..< lines.count {
+        if lines[i] > lines[i-1] {
+            increased += 1
+        }
     }
-    scoreboard
-        .sorted { lhs, rhs in
-            lhs.value > rhs.value
-        }
-        .forEach { name, score in
-            print("\(name) \(score) pts")
-        }
+    print(increased)
 }
 
-func parseLine(_ line: String) -> (name: String, score: Int) {
-    let helper = RegexHelper(pattern: #"([\-\w]*)\s(\d+)"#)
-    let result = helper.parse(line)
-    let name = result[0]
-    let score = Int(result[1])!
-    return (name: name, score: score)
+func main2() { // doh
+    let fileUrl = URL(fileURLWithPath: "./aoc-input")
+    guard let inputString = try? String(contentsOf: fileUrl, encoding: .utf8) else { fatalError("Invalid input") }
+    
+    let lines = inputString.components(separatedBy: "\n")
+        .filter { !$0.isEmpty }
+        .map { Int($0)! }
+    
+    var slidingWindows = [Int]()
+    for i in 0 ... lines.count - 3 {
+        slidingWindows.append(lines[i] + lines[i+1] + lines[i+2])
+    }
+    
+    var increased = 0
+    for i in 1 ..< slidingWindows.count {
+        if slidingWindows[i] > slidingWindows[i-1] {
+            increased += 1
+        }
+    }
+    print(increased)
 }
 
 main()
+main2()
